a6e9dc480a567d380946d4f9c0c7781480c95ecd
jaredsbror <electricalengineeringexpanded@gmail.com>
2025-04-18 16:49:44 -0600

    Preparing to copy .onxx files over

--- include/tts.h
+++ include/tts.h
@@ -39,7 +39,7 @@ PiperVoice piper_voices[] = {
     { "en_GB", "alan (medium)", "piper/piper-voices/en/en_GB/alan/medium/en_GB-alan-medium.onnx" },
     { "en_GB", "alba (medium)", "piper/piper-voices/en/en_GB/alba/medium/en_GB-alba-medium.onnx" },
     { "en_GB", "aru (medium)", "piper/piper-voices/en/en_GB/aru/medium/en_GB-aru-medium.onnx" },
-    { "en_GB", "jenny_dioco (medium)", "../piper/piper-voices/en/en_GB/jenny_dioco/medium/en_GB-jenny_dioco-medium.onnx.json" },
+    { "en_GB", "jenny_dioco (medium)", "../piper/piper-voices/en/en_GB/jenny_dioco/medium/en_GB-jenny_dioco-medium.onnx" },
     { "en_GB", "northern_english_male (medium)", "piper/piper-voices/en/en_GB/northern_english_male/medium/en_GB-northern_english_male-medium.onnx" },
     { "en_GB", "semaine (medium)", "piper/piper-voices/en/en_GB/semaine/medium/en_GB-semaine-medium.onnx" },
     { "en_GB", "southern_english_female (low)", "piper/piper-voices/en/en_GB/southern_english_female/low/en_GB-southern_english_female-low.onnx" },
